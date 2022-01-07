5c7a731f62f679c152e89fa2b15d410172d254ed
FlamingClappz <80445075+FlamingClappz@users.noreply.github.com>
2022-01-06 23:42:17 -0500

    Added VoiceUpdate event

---
+++ src/net/pgfmc/bot/events/VoiceUpdate.java
@@ -0,0 +1,28 @@
+package net.pgfmc.bot.events;
+
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.guild.voice.GenericGuildVoiceUpdateEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+import net.pgfmc.bot.Main;
+
+public class VoiceUpdate implements EventListener {
+	
+	@Override
+	public void onEvent(GenericEvent event) {
+		if (!(event instanceof GenericGuildVoiceUpdateEvent)) return;
+		GenericGuildVoiceUpdateEvent e = (GenericGuildVoiceUpdateEvent) event;
+		String Member = e.getMember().getUser().getName();
+		
+		if (event instanceof GuildVoiceJoinEvent)
+		{ 
+		  String channelName = e.getChannelJoined().getName();
+		  Main.JDA.getTextChannelById("721951670132801596").sendMessage(Member + "has left" + channelName);
+		} else if (event instanceof GuildVoiceLeaveEvent)
+		{
+		  String channelName = e.getChannelLeft().getName();
+		  Main.JDA.getTextChannelById("721951670132801596").sendMessage(Member + "has joined" + channelName);
+		}
+	}
+}
